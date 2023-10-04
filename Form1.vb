4a72704afa199cccd6dedccd73e01707f715ebb7
Lenovo-User <Lenovo-User@DESKTOP-Q92463H>
2023-10-04 20:21:25 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+        Dim text1, text2, text3, text4 As String
+        text1 = "Visual"
+        text2 = "basic"
+        text3 = "2022"
+        text4 = text1 + " " + text2 + " " + text3
+        MsgBox(text4)
+        Me.BackColor = Color.DarkOrchid
+    End Sub
+End Class
